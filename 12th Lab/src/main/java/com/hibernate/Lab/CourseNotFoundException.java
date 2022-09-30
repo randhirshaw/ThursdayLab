3074112d3d92f76a26beea8bcdba4b499d5c2b04
Randhir Shaw <86173761+randhirshaw@users.noreply.github.com>
2022-09-30 19:28:35 +0530

    Delete 12th Lab/src directory

--- 12th Lab/src/main/java/com/hibernate/Lab/CourseNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hibernate.Lab;
-
-public class CourseNotFoundException extends Exception{
-	public String getMessage(int id)
-	{
-		return "Course Not found with id "+id;
-	}
-}
--- 12th Lab/src/main/java/com/hibernate/Lab/InstructorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hibernate.Lab;
-
-public class InstructorNotFoundException extends Exception{
-	public String getMessage(int id)
-	{
-		return "Instructor Not found with id "+id;
-	}
-}
